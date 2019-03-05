f16ea3b2f6b69785edb9843a185ffdeec7eaae02
Naomi <n.saphra@ed.ac.uk>
2019-03-05 14:50:48 +0000

    add top level lstm staff

--- acd/scores/cd.py
+++ acd/scores/cd.py
@@ -4,7 +4,10 @@ from copy import deepcopy
 import numpy as np
 from scipy.special import expit as sigmoid
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 # propagate a three-part
 def propagate_three(a, b, c, activation):
     a_contrib = 0.5 * (activation(a + c) - activation(c) + activation(a + b + c) - activation(b + c))
@@ -210,6 +213,77 @@ def cd_text(batch, model, start, stop):
 
     return scores
 
+def cd_lstm(lstm, word_vecs, start, stop, cell_state=None):
+    word_vecs = word_vecs[:, 0].data
+    weights = lstm.state_dict()
+
+    # Index one = word vector (i) or hidden state (h), index two = gate
+    W_ii, W_if, W_ig, W_io = np.split(weights['weight_ih_l0'], 4, 0)
+    W_hi, W_hf, W_hg, W_ho = np.split(weights['weight_hh_l0'], 4, 0)
+    b_i, b_f, b_g, b_o = np.split(weights['bias_ih_l0'].cpu().numpy() + weights['bias_hh_l0'].cpu().numpy(), 4)
+    T = word_vecs.size(0)
+    hidden_dim = word_vecs.size(-1)
+    relevant = np.zeros((T, hidden_dim))
+    if cell_state is not None:
+        irrelevant = cell_state
+    else:
+        irrelevant = np.zeros((T, hidden_dim))
+    relevant_h = np.zeros((T, hidden_dim))
+    irrelevant_h = np.zeros((T, hidden_dim))
+    for i in range(T):
+        if i > 0:
+            prev_rel_h = relevant_h[i - 1]
+            prev_irrel_h = irrelevant_h[i - 1]
+        else:
+            prev_rel_h = np.zeros(hidden_dim)
+            prev_irrel_h = np.zeros(hidden_dim)
+
+        rel_i = np.dot(W_hi, prev_rel_h)
+        rel_g = np.dot(W_hg, prev_rel_h)
+        rel_f = np.dot(W_hf, prev_rel_h)
+        rel_o = np.dot(W_ho, prev_rel_h)
+        irrel_i = np.dot(W_hi, prev_irrel_h)
+        irrel_g = np.dot(W_hg, prev_irrel_h)
+        irrel_f = np.dot(W_hf, prev_irrel_h)
+        irrel_o = np.dot(W_ho, prev_irrel_h)
+
+        if i >= start and i <= stop:
+            rel_i = rel_i + np.dot(W_ii, word_vecs[i])
+            rel_g = rel_g + np.dot(W_ig, word_vecs[i])
+            rel_f = rel_f + np.dot(W_if, word_vecs[i])
+            rel_o = rel_o + np.dot(W_io, word_vecs[i])
+        else:
+            irrel_i = irrel_i + np.dot(W_ii, word_vecs[i])
+            irrel_g = irrel_g + np.dot(W_ig, word_vecs[i])
+            irrel_f = irrel_f + np.dot(W_if, word_vecs[i])
+            irrel_o = irrel_o + np.dot(W_io, word_vecs[i])
+
+        rel_contrib_i, irrel_contrib_i, bias_contrib_i = propagate_three(rel_i, irrel_i, b_i, sigmoid)
+        rel_contrib_g, irrel_contrib_g, bias_contrib_g = propagate_three(rel_g, irrel_g, b_g, np.tanh)
+
+        relevant[i] = rel_contrib_i * (rel_contrib_g + bias_contrib_g) + bias_contrib_i * rel_contrib_g
+        irrelevant[i] = irrel_contrib_i * (rel_contrib_g + irrel_contrib_g + bias_contrib_g) + (rel_contrib_i + bias_contrib_i) * irrel_contrib_g
+
+        if i >= start and i < stop:
+            relevant[i] += bias_contrib_i * bias_contrib_g
+        else:
+            irrelevant[i] += bias_contrib_i * bias_contrib_g
+
+        if i > 0:
+            rel_contrib_f, irrel_contrib_f, bias_contrib_f = propagate_three(rel_f, irrel_f, b_f, sigmoid)
+            relevant[i] += (rel_contrib_f + bias_contrib_f) * relevant[i - 1]
+            irrelevant[i] += (rel_contrib_f + irrel_contrib_f + bias_contrib_f) * irrelevant[i - 1] + irrel_contrib_f * \
+                                                                                                      relevant[i - 1]
+
+        o = sigmoid(np.dot(W_io, word_vecs[i]) + np.dot(W_ho, prev_rel_h + prev_irrel_h) + b_o)
+        rel_contrib_o, irrel_contrib_o, bias_contrib_o = propagate_three(rel_o, irrel_o, b_o, sigmoid)
+        new_rel_h, new_irrel_h = propagate_tanh_two(relevant[i], irrelevant[i])
+        # relevant_h[i] = new_rel_h * (rel_contrib_o + bias_contrib_o)
+        # irrelevant_h[i] = new_rel_h * (irrel_contrib_o) + new_irrel_h * (rel_contrib_o + irrel_contrib_o + bias_contrib_o)
+        relevant_h[i] = o * new_rel_h
+        irrelevant_h[i] = o * new_irrel_h
+
+    return relevant_h, irrelevant_h
 
 # this implementation of cd is very long so that we can view CD at intermediate layers
 # in reality, this should be a loop which uses the above functions
